eacc1e53103d3681fc01d3f1e64ce56f41133b81
Sebastian Schmidt <isibboi@gmail.com>
2017-03-31 15:46:58 +0300

    Implemented FMI weather download for Espoo

--- record_data.py
+++ record_data.py
@@ -2,11 +2,12 @@
 
 from wr.adapters.RDMAdapter import RDMAdapter
 from wr.adapters.DWDAdapter import DWDAdapter
+from wr.adapters.FMIAdapter import FMIAdapter
 import wr.db_model as db
 
 
 def record_data():
-    adapters = [RDMAdapter('RDM'), DWDAdapter('DWD')]
+    adapters = [RDMAdapter('RDM'), DWDAdapter('DWD'), FMIAdapter('FMI')]
 
     for adapter in adapters:
         adapter.receive_data()
